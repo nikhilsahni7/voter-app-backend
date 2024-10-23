a02f034703f6a705be5c0ddada54218eb8b14281
Nikhil Sahni <nikhil.sahni321@gmail.com>
2024-10-23 12:08:27 +0530

    "cors issue fixed"

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,14 @@ import { generateAnalysis } from "./utils/analysis";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
